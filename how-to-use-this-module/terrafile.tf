5ae8c14b71c422b93eda89c9799f10770eaf2275
Ezzio Moreira <enzziom@gmail.com>
2022-07-16 16:31:47 -0300

    fix: remove terraform backend
    Co-authored-by: Rafael Gomes <rgomes@dnsfilter.com>

--- how-to-use-this-module/terrafile.tf
+++ how-to-use-this-module/terrafile.tf
@@ -2,13 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "appmetalcorp"
-    key    = "iac/aws-ecr.tfstate"
-    region = "us-east-2"
-  }
-}
 
 
 module "ecr-mentoria" {
